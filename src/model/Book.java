6f9575d9b06c45268dceaf9e0724fc4f971a49c6
ca1phas <65637919+ca1phas@users.noreply.github.com>
2025-07-20 14:57:12 +0800

    data files and data models

---
+++ src/model/Book.java
@@ -0,0 +1,75 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private String genre;
+    private LocalDate releasedDate;
+    private String filename;
+
+    public Book(int id, String title, String author, String genre, LocalDate releasedDate, String filename) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.releasedDate = releasedDate;
+        this.filename = filename;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public LocalDate getReleasedDate() {
+        return releasedDate;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    // Setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setReleasedDate(LocalDate releasedDate) {
+        this.releasedDate = releasedDate;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    @Override
+    public String toString() {
+        return id + ": " + title + " by " + author
+                + "in genre " + genre
+                + ", released on " + releasedDate
+                + ". File: " + filename;
+    }
+}
---
+++ src/model/Reservation.java
@@ -0,0 +1,75 @@
+package model;
+
+import util.ReservationStatus;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Reservation {
+    private int id;
+    private int bookId;
+    private String username;
+    private LocalDateTime reservationDate;
+    private ReservationStatus status;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Reservation(int id, int bookId, String username, LocalDateTime reservationDate,
+            ReservationStatus status, LocalDate startDate, LocalDate endDate) {
+        this.id = id;
+        this.bookId = bookId;
+        this.username = username;
+        this.reservationDate = reservationDate;
+        this.status = status;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public LocalDateTime getReservationDate() {
+        return reservationDate;
+    }
+
+    public ReservationStatus getStatus() {
+        return status;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    // Setters
+    public void setStatus(ReservationStatus status) {
+        this.status = status;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    @Override
+    public String toString() {
+        return id + ": Book " + bookId
+                + " reserved by " + username + " (" + status.toString() + ") on "
+                + reservationDate + ", from " + startDate + " to " + endDate + ".";
+    }
+}
