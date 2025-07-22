7e54c02fcf15c8da5d1b047005b6b7b1b2a18dee
ca1phas <65637919+ca1phas@users.noreply.github.com>
2025-07-22 11:07:26 +0800

    update App.java

--- src/App.java
+++ src/App.java
@@ -1,8 +1,11 @@
+import model.User;
+
 public class App {
     public static void main(String[] args) throws Exception {
         // Initialize the system
         System.out.println("Welcome to the Library E-Book Lending & Reservation System!");
 
+        User currentUser = null;
         boolean running = true;
         while (running) {
             // Authenticate user
@@ -12,6 +15,14 @@ public class App {
             System.out.println("1. My Account");
             System.out.println("2. My Reservations");
             System.out.println("3. Books");
+
+            // Admin
+            System.out.println("4. Reservations");
+            System.out.println("5. Users");
+            System.out.println("4/6. Logout");
+
+            currentUser = null;
+            continue;
         }
 
         System.out.println("Thank you for using the Library E-Book Lending & Reservation System. Goodbye!");
